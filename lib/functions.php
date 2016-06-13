0bd86aa89e7220b9a9bdafc5bacd411a97e00bde
Brian Weaver <brian.weaver@northpoint.org>
2016-06-13 15:36:21 -0400

    Cleans up rewriting cdn urls

--- lib/functions.php
+++ lib/functions.php
@@ -4,33 +4,14 @@
 /**
  * Rewrite asset URLs on the fly to pull from CDN.
  */
-function cfcdn_rewrite_on_fly( $content ){
+function cfcdn_rewrite_on_fly( $content ) {
+  $uploads_url = wp_upload_dir()['baseurl'];
+  $cdn_public_url = CFCDN_CDN::settings()['public_url'];
 
-  $uploads = wp_upload_dir();
-  $uploads_url = str_replace( array('http://', 'https://'), '', $uploads['baseurl'] );
-
-  $cdn_settings = CFCDN_CDN::settings();
-  $public_url = str_replace( array('http://', 'https://'), '', $cdn_settings['public_url'] );
-
-  return str_replace( $uploads_url, $public_url, $content );
-
-}add_filter( "the_content", "cfcdn_rewrite_on_fly" );
-
-
-
-/**
- * Rewrite attachment URL to pull from CDN.
- */
-function cfcdn_rewrite_attachment_url( $url ){
-
-  $uploads = wp_upload_dir();
-  $uploads_url = str_replace( array('http://', 'https://'), '', $uploads['baseurl'] );
-
-  $cdn_settings = CFCDN_CDN::settings();
-  $public_url = str_replace( array('http://', 'https://'), '', $cdn_settings['public_url'] );
-  return str_replace( $uploads_url, $public_url, $url );
-
-}add_filter('wp_get_attachment_url', 'cfcdn_rewrite_attachment_url');
+  return str_replace( $uploads_url, $cdn_public_url, $content );
+}
+add_filter( "the_content", "cfcdn_rewrite_on_fly" );
+add_filter('wp_get_attachment_url', 'cfcdn_rewrite_on_fly');
 
 
 
@@ -39,7 +20,8 @@ function cfcdn_rewrite_attachment_url( $url ){
  */
 function cfcdn_send_to_cdn_on_attachment_post_save( $post_id ){
   CFCDN_Util::upload_all();
-}add_action( 'add_attachment', 'cfcdn_send_to_cdn_on_attachment_post_save' );
+}
+add_action( 'add_attachment', 'cfcdn_send_to_cdn_on_attachment_post_save' );
 
 
 
@@ -50,7 +32,8 @@ function cfcdn_admin_page_load() {
   if( is_admin() ){
     CFCDN_Util::upload_all();
   }
-}add_action( 'shutdown', 'cfcdn_admin_page_load' );
+}
+add_action( 'shutdown', 'cfcdn_admin_page_load' );
 
 
 ?>
